8633f3a57e57dc49792cbbf0cadd8e66679ddfd3
Otto Moen <ottmo242@student.liu.se>
2020-09-08 09:53:22 +0200

    Added the datafile with documentation

---
+++ Lab1HugoOtto/R/data.R
@@ -0,0 +1,12 @@
+#' A weighted graph.
+#' 
+#' A dataset containing information on edges in a graph and their weight.
+#' 
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'  \item{v1}{nodes on one side of an edge}
+#'  \item{v2}{nodes on the other side of the edge}
+#'  \item{w}{the weight of the edge}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
+"wiki_graph"
