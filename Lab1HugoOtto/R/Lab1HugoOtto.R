2ab9881b97597bb1bd54a2190a4a3396b52d6e59
Otto Moen <ottmo242@student.liu.se>
2020-09-08 08:36:14 +0200

    Created package documentation

---
+++ Lab1HugoOtto/R/Lab1HugoOtto.R
@@ -0,0 +1,15 @@
+#' Lab1HugoOtto: A package containing the functions euclidean and dijkstra
+#' 
+#' This package contains two functions:
+#' euclidean and dijkstra.
+#' 
+#' @section Euclidean:
+#' The euclidean function takes two numbers and returns the greatest common divisor using the euclidean algorithm.
+#' 
+#' @section Dijkstra:
+#' The dijkstra function takes a graph and an inital node and returns the shortest path from the inital node to every other node in the graph using Dijkstra's algorithm.
+#' 
+#' @docType package
+#' @name Lab1HugoOtto
+NULL
+#> NULL
