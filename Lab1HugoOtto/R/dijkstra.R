9d41a8f97680d04de0ff50f400c33d40d1712bc2
hugkn566 <70522944+hugkn566@users.noreply.github.com>
2020-09-08 08:39:26 +0200

    references

--- Lab1HugoOtto/R/dijkstra.R
+++ Lab1HugoOtto/R/dijkstra.R
@@ -2,8 +2,9 @@
 #' 
 #' @param graph A data.frame with three variables containing the edges of a graph and the weight of the edge.
 #' @param init_node A node within \code{graph} from which to calculate the shortest path to every other node.
-#' @details This function computes Dijkstra's algorithm to find the shortest paths between nodes in a graph (\href{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}{Wikipedia}). 
+#' @details This function computes Dijkstra's algorithm to find the shortest paths between nodes in a graph. 
 #' @return A vector containing the shortest paths from \code{init_node} to every other node in the graph.
+#' @references https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm (\href{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}{Wikipedia}).
 #' @examples
 #' dijkstra(wiki_graph, 1)
 #' dijkstra(wiki_graph, 3) 
--- Lab1HugoOtto/R/euclidean.R
+++ Lab1HugoOtto/R/euclidean.R
@@ -1,9 +1,9 @@
 #' Compute the euclidean algorithm for two numbers 
 #'  
 #' @param (a,b) Numeric scalar or integer.
-#' @details This function computes euclidean algorithm to find the greatest common divisor between two integers (\href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{Wikipedia}). 
-
+#' @details This function computes euclidean algorithm to find the greatest common divisor between two integers. 
 #' @return The greatest common divisor of \code{a} and \code{b}. 
+#' @references https://en.wikipedia.org/wiki/Euclidean_algorithm (\href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{Wikipedia}).
 #' @examples  
 #' euclidean(123612, 13892347912) 
 #' euclidean(100, 1000) 
