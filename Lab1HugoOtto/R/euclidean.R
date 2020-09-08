43dfa3a9dc7627fda82ac1f0f03b9ea8583bb49d
hugkn566 <70522944+hugkn566@users.noreply.github.com>
2020-09-08 08:14:25 +0200

    roxygen2 eucledian

--- Lab1HugoOtto/R/euclidean.R
+++ Lab1HugoOtto/R/euclidean.R
@@ -1,3 +1,11 @@
+#' Compute the euclidean algorithm for two numbers 
+#'  
+#' @param c(a,b) Numeric scalar or integer. 
+#' @return The greatest common divisor of \code{a} and \code{b}. 
+#' @examples  
+#' euclidean(123612, 13892347912) 
+#' euclidean(100, 1000) 
+
 euclidean <- function(a, b) {
   while (b != 0) {
     t <- b
@@ -6,5 +14,7 @@ euclidean <- function(a, b) {
   }
   return(a)
 }
+
+
 
 
